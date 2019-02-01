56f1c666c9e82211446ae001e0030c2d06ec6f94
ayang223 <ayang223@users.noreply.github.com>
2019-02-01 12:52:14 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Navbar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    // counters = a state property, each counter has id proeprty and value property
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id != counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCoutners={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container" />
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onIncrement={this.handleIncrement}
+          onDelete={this.handleDelete}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
